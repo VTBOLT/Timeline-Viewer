9d3a9f9ed29c6472193cf26f98f2506a39a4bceb
Mason DiGiorgio <mason@digiorgiofamily.net>
2025-11-13 15:06:24 -0500

    Whoops am silly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TaskListComponent],
+  imports: [TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   animations: [
